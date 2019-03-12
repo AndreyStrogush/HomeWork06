dd9e8be1384956aac431a6a0fd686c326a2897db
Andrey Strogush <magistrogush@gmail.com>
2019-03-12 16:58:16 +0200

    third task

--- src/com/flowers/Main.java
+++ src/com/flowers/Main.java
@@ -1,25 +1,27 @@
 package com.flowers;
-import java.util.Arrays;
+
+import java.io.IOException;
 import java.util.stream.Stream;
 
 public class Main {
+    private static String fileName = "C://Users/Admin/IdeaProjects/HomeWork06/src/com/flowers//testFlower.txt";
+
     public static void main(String[] args) {
         FlowerStore flowerStore = new FlowerStore();
         System.out.println("In wallet is now: " + flowerStore.getCash() + "$");
         Flower[] flowersHeap = flowerStore.sell(5, 3, 1);
         Stream.of(flowersHeap).forEach(System.out::print);
-        System.out.println();
-        System.out.println("In wallet is now: " + flowerStore.getCash() + "$");
+        System.out.println("\nIn wallet is now: " + flowerStore.getCash() + "$");
         Flower[] flowersBuckets = flowerStore.sellSequence(5, 3, 11);
         Stream.of(flowersBuckets).forEach(System.out::print);
-        System.out.println();
-        System.out.println("In wallet is now: " + flowerStore.getCash() + "$");
-
-//        System.out.println(Arrays.toString(flowersHeap));
-//        System.out.println(Arrays.toString(flowersBuckets));
-//        Stream.of(flowersHeap).forEach(item-> {
-//            System.out.print(item);
-//            System.out.println(FlowerStore.wallet());
-//        });
+        System.out.println("\nIn wallet is now: " + flowerStore.getCash() + "$");
+        try {
+            Flower[] flowersFromFile = FlowersLoader.load(fileName);
+            Stream.of(flowersFromFile).forEach(System.out::print);
+            System.out.println("\nIn wallet is now: " + flowerStore.getCash() + "$");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
